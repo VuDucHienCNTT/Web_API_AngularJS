0dd8831e76389f92c1e17d319e2be4fb90646443
VuDucHienCNTT <vuhienit@gmail.com>
2018-04-10 14:11:22 +0700

    o

--- TeduShop.Service/PostCategoryService.cs
+++ TeduShop.Service/PostCategoryService.cs
@@ -51,7 +51,6 @@ namespace TeduShop.Service
         public IEnumerable<PostCategory> GetAllByParentId(int parentId)
         {
             return _postCategoryReporsitory.GetMulti(x => x.Status && x.ParentID == parentId);
-
         }
 
         public PostCategory GetById(int id)
