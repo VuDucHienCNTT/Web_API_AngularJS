f8d6f003bacdbaefe1a78a2093d9a4bd08df5737
VuDucHienCNTT <vuhienit@gmail.com>
2018-04-11 16:06:07 +0700

    add productservice

---
+++ TeduShop.Service/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IProductService
+    {
+        void Add(Product product);
+
+        void Update(Product product);
+
+        void Delete(int id);
+
+        IEnumerable<Product> GetAll();
+
+        IEnumerable<Product> GetAll(string keywork);
+    }
+
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ProductService(IProductRepository productService, IUnitOfWork unitOfWork)
+        {
+            this._productRepository = productService;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(Product product)
+        {
+            _productRepository.Add(product);
+        }
+
+        public void Delete(int id)
+        {
+            _productRepository.Delete(id);
+        }
+
+        public IEnumerable<Product> GetAll(string keywork)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _productRepository.GetAll();
+        }
+
+        public void Update(Product product)
+        {
+            _productRepository.Update(product);
+        }
+    }
+}
