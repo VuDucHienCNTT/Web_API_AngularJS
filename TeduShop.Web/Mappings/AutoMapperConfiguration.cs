0a04b2157f7d9ccfe269fe6527432acded673d89
VuDucHienCNTT <vuhienit@gmail.com>
2018-04-12 01:02:09 +0700

    add code api,  automapper viewmodel

---
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<List<Post>, List<PostViewModel>>();
+                cfg.CreateMap<List<Tag>, List<TagViewModel>>();
+                cfg.CreateMap<List<PostCategory>, List<PostCategoryViewModel>>();
+            });
+        }
+    }
+}
