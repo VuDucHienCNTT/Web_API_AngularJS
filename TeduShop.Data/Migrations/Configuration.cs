4dcb3ef98d840d3620fcf2612d0f854678d13a52
VuDucHienCNTT <vuhienit@gmail.com>
2018-04-13 00:31:39 +0700

    add tbAcc, Angularjs

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace TeduShop.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TeduShop.Model.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TeduShop.Data.TeduShopDbContext>
     {
@@ -18,6 +21,35 @@ namespace TeduShop.Data.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            //  This method will be called after migrating to the latest version.
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "hienvd1",
+                Email = "vuhienit@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "HienVD1 FSoft"
+
+            };
+
+            manager.Create(user, "abcd12345-");
+
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+            var adminUser = manager.FindByEmail("vuhienit@gmail.com");
+
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
         }
     }
 }
---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+//create module
+var myApp = angular.module('myModule', [])
+myApp.controller("schoolController", schoolController);
+
+function schoolController($scope) {
+    $scope.message = "This is Hien from school"
+}
+
+//myApp.controller("studentController", studentController);
+//myApp.controller("teacherController", teacherController);
+
+
+////myController.$inject = ['$scope'];
+
+
+////
+//function schoolController($scope) {
+//    $scope.message = "This is Hien from school"
+//}
+
+//function studentController($scope) {
+//    //$scope.message = "This is Hien from student"
+//}
+
+//function teacherController($scope) {
+//    //$scope.message = "This is Hien from teacher"
+//}
