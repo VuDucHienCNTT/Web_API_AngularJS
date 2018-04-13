4fa81048851e05672840fdd3dbd97b080ee4bfc6
VuDucHienCNTT <vuhienit@gmail.com>
2018-04-13 17:29:21 +0700

    angular

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -1,28 +1,19 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 //create module
 var myApp = angular.module('myModule', [])
+
 myApp.controller("schoolController", schoolController);
-
-function schoolController($scope) {
-    $scope.message = "This is Hien from school"
-}
-
 //myApp.controller("studentController", studentController);
-//myApp.controller("teacherController", teacherController);
 
 
 ////myController.$inject = ['$scope'];
 
+//
+function schoolController($scope) {
+    $scope.message = "School";
+    $scope.message1 = "ST";
+    $scope.btnCheck = function () {
+        $scope.message1 = "Student";
 
-////
-//function schoolController($scope) {
-//    $scope.message = "This is Hien from school"
-//}
-
-//function studentController($scope) {
-//    //$scope.message = "This is Hien from student"
-//}
-
-//function teacherController($scope) {
-//    //$scope.message = "This is Hien from teacher"
-//}
+    }
+}
